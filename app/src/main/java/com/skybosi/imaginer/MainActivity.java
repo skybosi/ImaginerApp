32703166e67d820e4b13692b439e97d056138ccb
skybosi <skybosi_ok@163.com>
2016-08-23 22:39:37 +0800

    First push someting,can open files and load picture

---
+++ app/src/main/java/com/skybosi/imaginer/MainActivity.java
@@ -0,0 +1,178 @@
+package com.skybosi.imaginer;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Handler;
+import android.os.Message;
+
+import java.util.Random;
+
+public class MainActivity extends Activity implements View.OnClickListener, View.OnLongClickListener{
+    String bmpFile = null;
+    Handler mHandler = null;
+    private final static int REQUEST_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SurfaceView sfv = (SurfaceView) findViewById(R.id.surface);
+        ImageButton ib = (ImageButton) findViewById(R.id.openSD);
+        Button bt = (Button)findViewById(R.id.nextPoint);
+        ib.setOnClickListener(this);
+        bt.setOnLongClickListener(this);
+        mHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (msg.what) {
+                    case 0:
+                        Toast.makeText(getApplicationContext(), "You choose is not a picture!", Toast.LENGTH_LONG).show();
+                    default:
+                        break;
+                }
+            }
+        };
+        final SurfaceHolder holder = sfv.getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder arg0) {
+                if (thread != null)
+                    thread.mystop();
+            }
+
+            @Override
+            public void surfaceCreated(SurfaceHolder arg0) {
+                if (bmpFile != null) {
+                    thread = new MyThread(arg0);
+                    thread.start();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
+
+    MyThread thread = null;
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.openSD) {
+            loadFile();
+        }
+    }
+
+    private void loadFile() {
+        Intent intent = new Intent("android.intent.action.FILE");
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        if (v.getId() == R.id.nextPoint) {
+            //String step = null;
+            final EditText inputServer = new EditText(this);
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Next Step Set").setIcon(android.R.drawable.ic_dialog_info).setView(inputServer)
+                    .setNegativeButton("CANCEL", null);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    inputServer.getText().toString();
+                    ((Button)findViewById(R.id.nextPoint)).setText("NEXT" + "(" + inputServer.getText().toString() + ")" );
+                }
+            });
+            builder.show();
+        }
+
+        return false;
+    }
+
+
+    class MyThread extends Thread {
+        SurfaceHolder holder;
+        boolean refresh = true;
+        Random random = new Random(0);
+
+        public MyThread(SurfaceHolder holder) {
+            this.holder = holder;
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            Canvas canvas = holder.lockCanvas();
+            Bitmap bm = null;
+            bm = BitmapFactory.decodeFile(bmpFile);
+            if (bm == null && !bmpFile.isEmpty()) {
+                mHandler.sendEmptyMessage(0);
+                holder.unlockCanvasAndPost(canvas);
+            }
+            int left = (canvas.getWidth() - bm.getWidth())/2;
+            int top =  (canvas.getHeight() - bm.getHeight())/2;
+            canvas.drawBitmap(bm, left, top, new Paint());
+            holder.unlockCanvasAndPost(canvas);
+        }
+
+        void mystop() {
+            refresh = false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == FileView.RESULT_CODE) {
+                Bundle bundle = data.getExtras();
+                bmpFile = bundle.getString("filename");
+                //Toast.makeText(MainActivity.this, bmpFile, Toast.LENGTH_LONG).show();
+                ((TextView)findViewById(R.id.showFilepath)).setText(bmpFile);
+            }
+        }
+    }
+
+}
+
+            /*
+            while(refresh){
+                Canvas canvas = holder.lockCanvas();
+                int left = random.nextInt(canvas.getWidth());
+                int top =  random.nextInt(canvas.getHeight());
+
+                Bitmap bm = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+                canvas.drawBitmap(bm, left, top, new Paint());
+                holder.unlockCanvasAndPost(canvas);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            */
